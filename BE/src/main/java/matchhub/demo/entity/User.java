c7fa935842fd332f2fc5bba6bc974853826bdfd2
NobleBobby <hoangthienkg12345@gmail.com>
2025-09-22 09:58:12 +0700

    First commit

---
+++ BE/src/main/java/matchhub/demo/entity/User.java
@@ -0,0 +1,16 @@
+package matchhub.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+}
