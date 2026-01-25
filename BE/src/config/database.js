c294d3a27697becb75f26db47d7bb891d0d6308b
Hoang <hoangthienkg12345@gmail.com>
2026-01-26 00:06:36 +0700

    fix be

--- BE/src/config/database.js
+++ BE/src/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/matchhub');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://matchhub_manager:matchhub12345@cluster0.ydvoups.mongodb.net/matchhub');
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error connecting to MongoDB: ${error.message}`);
