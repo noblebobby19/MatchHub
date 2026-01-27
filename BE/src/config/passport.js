7bdaaa686eb5b5c331ab4039dd7388ecb7b2d7f1
noblebobby19 <hoangthienkg12345@gmail.com>
2026-01-27 10:22:22 +0700

    Update passport.js

--- BE/src/config/passport.js
+++ BE/src/config/passport.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/auth/google/callback',
+            callbackURL: '${process.env.BACKEND_URL}/api/auth/google/callback',
             scope: ['profile', 'email'],
         },
         async (accessToken, refreshToken, profile, done) => {
