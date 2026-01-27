6327bf76dcfbbd20bad3fb032b93ede7e2c7533d
Hoang <hoangthienkg12345@gmail.com>
2026-01-27 10:45:47 +0700

    update

--- BE/src/config/passport.js
+++ BE/src/config/passport.js
@@ -10,8 +10,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/auth/google/callback',
+            callbackURL: `${process.env.BACKEND_URL?.replace(/\/$/, '')}/api/auth/google/callback`,
             scope: ['profile', 'email'],
+            proxy: true,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
